49feae6548f648dafd324507936e96d1d16e0698
kristian.aranda@gmail.com <kristian.aranda@gmail.com>
2024-04-24 09:13:30 +0200

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjkxNzI2/spectral.js?branch_slug=main&token=403a6fc4-68a1-4275-8785-4ef469950ec2";
+export default { extends: ruleset };
